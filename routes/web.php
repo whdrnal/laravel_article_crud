9cea8def38a94e399567238a4317c2e190e3d91e
whdrnal <whdrnal34@gmail.com>
2022-02-20 20:49:30 +0900

    Article 관리자 기능

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+# Url Post , Get 요청 name() view 페이지나 컨트롤러에서 route를 호출
 Route::get('/', function () {
     return view('welcome');
 });
